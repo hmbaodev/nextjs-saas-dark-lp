9fa7e5691a891c05f03c38569b75ef80bc7a06e1
Hua Minh Bao <hmbaodev>
2025-12-18 21:56:43 +0700

    feat: add hero ui

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,3 +1,54 @@
+import Image from "next/image";
+
+import Button from "@/components/Button";
+import Pointer from "@/components/Pointer";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
+
 export default function Hero() {
-    return <div>Hero</div>;
+  return (
+    <section className="py-24 overflow-x-clip">
+      <div className="container relative">
+        <div className="absolute -left-32 top-16 hidden lg:block">
+          <Image src={designExample1Image} alt="Design Example 1" />
+        </div>
+        <div className="absolute -right-64 -top-16 hidden lg:block">
+          <Image src={designExample2Image} alt="Design Example 2" />
+        </div>
+        <div className="absolute left-56 top-96 hidden lg:block">
+          <Pointer name="Andrea" color="blue" />
+        </div>
+        <div className="absolute right-80 -top-4 hidden lg:block">
+          <Pointer name="Bryan" color="red" />
+        </div>
+        <div className="flex justify-center">
+          <div className="inline-flex py-1 px-3 bg-linear-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+            ✨ $7.5 seed round raised
+          </div>
+        </div>
+        <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
+          Impactful design,<br />created effortlessly
+        </h1>
+        <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+          Design tools should&apos;n slow you down. Layers powerful features
+          with an intuitive interface that in your creative flow
+        </p>
+        <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="bg-transparent px-4 md:flex-1 border-none outline-none"
+          />
+          <Button
+            variant="primary"
+            type="submit"
+            className="whitespace-nowrap"
+            size="sm"
+          >
+            Sign Up
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
 }
