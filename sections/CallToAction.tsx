ceef78ca8e2b36b9702d0e582982035552dccd2d
Hua Minh Bao <hmbaodev>
2025-12-23 21:24:43 +0700

    feat: add CTA section animation

--- sections/CallToAction.tsx
+++ sections/CallToAction.tsx
@@ -1,15 +1,47 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { AnimationPlaybackControls, motion, useAnimate } from "motion/react";
+
 export default function CallToAction() {
+  const [isHovered, setIsHovered] = useState(false);
+  const animation = useRef<AnimationPlaybackControls>(null);
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    animation.current = animate(
+      scope.current,
+      { x: "-50%" },
+      { duration: 30, ease: "linear", repeat: Infinity }
+    );
+
+    animation.current.speed = 0.5;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (animation.current) {
+      if (isHovered) animation.current.speed = 0.5;
+      else animation.current.speed = 1;
+    }
+  }, [isHovered]);
+
   return (
     <section className="py-24">
       <div className="overflow-x-clip p-4 flex">
-        <div className="flex flex-none gap-16 text-7xl font-medium">
+        <motion.div
+          ref={scope}
+          className="flex flex-none gap-16 text-7xl md:text-8xl font-medium group"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           {Array.from({ length: 10 }).map((_, i) => (
             <div key={i} className="flex items-center gap-16">
               <span className="text-lime-400">&#10038;</span>
-              <span>Try it for free</span>
+              <span className="group-hover:text-lime-400">Try it for free</span>
             </div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
